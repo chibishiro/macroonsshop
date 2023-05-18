ea26660a1ba73c5c54feaaa7a29cc5bb09e254ad
in-Lan <bLadeboom17@gmail.com>
2023-05-18 15:25:28 +0500

    paragraph added

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById("click").addEventListener('click',function()
+{
+    this.style.display = 'none';
+});
+document.getElementById("click2").addEventListener('click',function()
+{
+    this.style.display = 'none';
+});
+document.getElementById("click3").addEventListener('click',function()
+{
+    this.style.display = 'none';
+});
+document.getElementById("click4").addEventListener('click',function()
+{
+    this.style.display = 'none';
+});
+document.getElementById("click5").addEventListener('click',function()
+{
+    this.style.display = 'none';
+});
+document.getElementById("click6").addEventListener('click',function()
+{
+    this.style.display = 'none';
+});
+document.getElementById("click7").addEventListener('click',function()
+{
+    this.style.display = 'none';
+});
